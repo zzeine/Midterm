5e21593ff9bb67677efce209a30b025461b95f12
zeinab <zzeine@umich.edu>
2025-02-27 15:10:33 -0500

    initial commit

---
+++ factorialcalculator.java
@@ -0,0 +1,12 @@
+public class factorialcalculator {
+    public static void main(String[] args) {
+        int num = 5;  // Predefined number to calculate factorial
+        int factorial = 1;
+
+        for (int i = 1; i <= num; i++) { // Loop through numbers
+            factorial *= i;  // Multiply current number
+        }
+
+        System.out.println("Factorial of " + num + " is: " + factorial);
+    }
+}
